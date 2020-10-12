bcde41b5ff5abb52ff2a01e14729390a68a38c43
nvhbao <65130454+nvhbao@users.noreply.github.com>
2020-10-11 22:50:06 -0500

    Add files via upload

---
+++ lab09/maze.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+    $("start").onclick = startClick;
+    $("end").onmouseover = overEnd;
+    $("maze").onmouseleave = noCheat;
+    
+    $(".boundary").mouseenter(function(){
+        overBoundary();
+      });
+    $("#maze").mouseleave(function(){
+        overBoundary();
+      });
+    /*
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].onmouseover = overBoundary;
+        boundaries[i].onmousleave = overBoundary;
+    }
+    */
+};
+
+function overBoundary() {
+    var x = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "red";
+        if (x[i].style.backgroundColor == "red") {
+            var s = document.getElementById("status").innerHTML = "You Lose!";
+        }
+    }
+}
+
+function startClick() {
+    var x = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "#eeeeee";
+        if (x[i].style.backgroundColor != "red") {
+            var s = document.getElementById("status").innerHTML = "Move your mouse over the 'S' to begin.";
+        }
+    }
+}
+
+function overEnd() {
+    var x = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < x.length; i++) {
+        if (x[i].style.backgroundColor != "red") {
+            var s = document.getElementById("status").innerHTML = "You win!";
+        }
+    }
+}
+
+function noCheat() {
+    var x = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "red";
+        if (x[i].style.backgroundColor == "red") {
+            var s = document.getElementById("status").innerHTML = "Nice Try...";
+        }
+    }
+}
